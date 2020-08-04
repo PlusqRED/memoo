9a531a8d1bf34d26fba4a7d3d2583fc94962bcb4
plusqRED <rickes.oleg@gmail.com>
2020-08-04 17:28:03 +0300

    initial commit

---
+++ src/main/java/com/grape/memoo/MemooApplication.java
@@ -0,0 +1,13 @@
+package com.grape.memoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemooApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grape/memoo/MemooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grape.memoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
